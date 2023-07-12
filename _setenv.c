a5ad4e0e51804a64861f1faa4d383977bb0272fc
root <chasenaidoo9@gmail.com>
2023-07-12 11:08:32 -0700

    Fixed merge conflict

--- _setenv.c
+++ _setenv.c
@@ -1,34 +1,6 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
- * _setenv - emulates setenv function
- *
- * @name: string representing environment variable
- * @value: value to be assigned to environment variable
- * @overwrite: integer flag for overwriting variables
- *
- * Return: 0 (success), -1 (error)
- *
- */
-int _setenv(const char *name, const char *value, int overwrite)
-{
-	int result;
-
-	if (name == NULL || value == NULL || name[0] == '\0' || value[0] == '\0')
-	{
-	write(STDERR_FILENO, "setenv: Invalid command syntax\n", 31);
-	return (-1);
-	}
-	result = _setenv(name, value, overwrite);
-
-	if (result != 0)
-	{
-	write(STDERR_FILENO, "setenv: Failed to set environment vaiable\n", 43);
-	return (-1);
-	}
-	return (0);
-=======
  * _setenv - Emulates setenv
  *
  * @name: String representing environment variable
@@ -54,5 +26,4 @@ int _setenv(const char *name, const char *value, int overwrite)
         return (-1);
         }
         return (0);
->>>>>>> 5db307598998b3bc92b11c6bdb5185edae3b5aa5
 }
--- parse_arguments.c
+++ parse_arguments.c
@@ -6,34 +6,12 @@
  *
  * @input: input string
  * @args: array to store arguments
-=======
- * parse_arguments - Tokenises input string and stores arguments in array
- *
- * @input: input string
- * @args: array to store arugments
->>>>>>> 5db307598998b3bc92b11c6bdb5185edae3b5aa5
  *
  * Return: number of arguments
  *
  */
 int parse_arguments(char *input, char **args)
 {
-<<<<<<< HEAD
-	int count = 0;
-	char *token;
-
-	token = strtok(input, " \t\n\r");
-
-	while (token != NULL && count < MAX_ARGS - 1)
-	{
-	args[count] = token;
-	count++;
-	token = strtok(NULL, " \t\n\r");
-	}
-	args[count] = NULL;
-
-	return (count);
-=======
         int count = 0;
         char *token;
 
@@ -48,5 +26,4 @@ int parse_arguments(char *input, char **args)
         args[count] = NULL;
 
         return (count);
->>>>>>> 5db307598998b3bc92b11c6bdb5185edae3b5aa5
 }
